25c199f55478787f7650baf2015eb786770c2d65
Efim <42698670+Efim27@users.noreply.github.com>
2023-03-27 15:44:51 +0300

    Header, SideNav

---
+++ frontend/src/lib/components/Header.svelte
@@ -0,0 +1,168 @@
+<script>
+    import {isSideMenuOpened} from "../shared/stores/side-menu.js";
+
+    export let contacts;
+</script>
+
+<header class="header">
+    <div class="header__inner">
+        <a href="/"
+        ><img
+                class="header__logo logo logo__image"
+                src="/logo2.png"
+                alt="Логотип"
+        /></a>
+        <nav class="header__nav main-nav">
+            <div class="main-nav__links">
+                <a href="/team" class="main-nav__link">наша команда</a>
+                <a href="/portfolio" class="main-nav__link">портфолио</a>
+                <a href="/contacts" class="main-nav__link">контакты</a>
+            </div>
+        </nav>
+        <div class="header__contacts header-contacts">
+            <a class="header-contacts__phone" href="tel:{contacts.phone}"
+            >{contacts.phone}</a
+            >
+        </div>
+        <div class="header__menu">
+            <div class="side-menu-btn">
+                <img
+                        src="/images/icons/burger.svg"
+                        alt="Меню"
+                        on:click={() => {isSideMenuOpened.set(true)}}
+                >
+            </div>
+            <div class="dropdown header-profile-dropdown">
+                <div class="dropdown__content header-profile-dropdown__content">
+                    <div class="header-profile-dropdown__filler"></div>
+                    <div class="header-profile-dropdown__inner">
+                        <a href="/courses" class="header-profile-dropdown__link"
+                        >Мои курсы</a
+                        >
+                        <a href="/settings" class="header-profile-dropdown__link"
+                        >Настройки профиля</a
+                        >
+                        <a href="#" class="header-profile-dropdown__link" data-request="onLogout"
+                           data-request-data="redirect: '/'"
+                        >Выход</a
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<style lang="scss">
+  @import 'src/styles/main.scss';
+
+  .header {
+    font-family: $font_main;
+    white-space: nowrap;
+    padding: 0 20px;
+
+    a {
+      text-decoration: none;
+    }
+
+    &__inner {
+      height: 130px;
+      max-width: $page_inner_width;
+      margin: 0 auto;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    &__logo {
+      max-width: 200px;
+    }
+
+    &__nav {
+      margin-left: 10px;
+    }
+
+    &__contacts {
+      margin-left: 10px;
+    }
+
+    &__menu {
+      display: flex;
+      margin-left: 10px;
+    }
+  }
+
+  .logo {
+  }
+
+  .main-nav {
+    &__links {
+      display: flex;
+    }
+
+    &__link {
+      @include text(p2);
+      margin-left: 50px;
+      opacity: 0.9;
+
+      &:nth-child(1) {
+        margin-left: 0px;
+      }
+    }
+  }
+
+  .header-contacts {
+    display: flex;
+
+    &__phone {
+      opacity: 1;
+      @include text(p2);
+      font-weight: normal;
+      opacity: 1;
+    }
+
+    &__soc-list {
+      display: flex;
+      align-items: center;
+      margin-left: 48px;
+    }
+
+    &__soc {
+      margin-left: 25px;
+
+      &:nth-child(1) {
+        margin-left: 0px;
+      }
+
+      img {
+        width: 17px;
+      }
+    }
+  }
+
+  .dropdown {
+    position: relative;
+
+    &__content {
+      position: absolute;
+      top: -9999em;
+      right: 0px;
+      opacity: 0;
+      z-index: 11;
+      transition: 0.3s opacity;
+    }
+
+    &:hover &__content,
+    &__content:hover {
+      top: 20px;
+      opacity: 1;
+      height: auto;
+    }
+  }
+
+  .side-menu-btn {
+    &:hover {
+      cursor: pointer;
+    }
+  }
+</style>
---
+++ frontend/src/lib/components/SideNav.svelte
@@ -0,0 +1,162 @@
+<script>
+    import {isSideMenuOpened} from '$lib/shared/stores/side-menu'
+
+    export let contacts;
+    export let socLinks;
+</script>
+
+<div class="side-menu {$isSideMenuOpened ? 'side-menu_opened' : ''}">
+    <div class="side-menu__content">
+        <div class="side-menu__close">
+            <img src="images/icons/close.png" alt="Закрыть боковое меню" on:click={() => {isSideMenuOpened.set(false)}}>
+        </div>
+        <div class="side-menu__links">
+            <a href="/team" class="side-menu__link">наша команда</a>
+            <a href="/portfolio" class="side-menu__link">портфолио</a>
+            <a href="/contacts" class="side-menu__link">контакты</a>
+        </div>
+        <div class="side-menu__contacts">
+            <div class="side-menu__phone-row phone-row">
+                <a href="tel:{contacts.phone}" class="phone-row__phone">{contacts.phone}</a>
+                {#if contacts.telegram}
+                    <a href="{contacts.telegram}" class="phone-row__soc-icon"><img
+                            src="/images/icons/soc/telegram.png" alt="Телеграм"></a>
+                {/if}
+            </div>
+            <div class="side-menu__email">
+                <a href="mailto:{contacts.email}">{contacts.email}</a>
+            </div>
+            <div class="side-menu__soc-links">
+                {#each socLinks as socLink }
+                    <a href="{socLink.link}" class="side-menu__soc-link soc-link">{socLink.name}</a>
+                {/each}
+            </div>
+        </div>
+    </div>
+</div>
+
+<style lang="scss">
+  @import 'src/styles/main.scss';
+
+  .side-menu {
+    width: 100%;
+    max-width: 900px;
+    height: 100vh;
+    padding: 0px 120px;
+    padding-top: 60px;
+    padding-bottom: 80px;
+    position: fixed;
+    right: 0px;
+    top: 0px;
+    background-color: #f5f5f5;
+    z-index: -1;
+    opacity: 0;
+
+    a {
+      text-decoration: none;
+    }
+
+    &_opened {
+      opacity: 1;
+      z-index: 1;
+      animation: fadeInFromNone 0.5s ease-out;
+    }
+
+    &_loaded {
+      z-index: 11;
+      opacity: 1;
+      display: none;
+    }
+
+    &__content {
+      position: relative;
+      padding-top: 30px;
+    }
+
+    &__close {
+      position: absolute;
+      right: 0px;
+      top: 0px;
+      cursor: pointer;
+
+      img {
+        width: 100%;
+        max-width: 20px;
+      }
+    }
+
+    &__links {
+    }
+
+    &__link {
+      @include text(p1);
+      font-size: 30px;
+      display: block;
+      transition: 0.5s all;
+
+      &:hover {
+        background: $gradient_main;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+      }
+    }
+
+    div &__phone-row {
+      margin-top: 65px;
+    }
+
+    &__email {
+      margin-top: 15px;
+
+      a {
+        @include text(p1);
+      }
+    }
+
+    &__soc-links {
+      display: flex;
+      margin: 0 -10px;
+      margin-top: 30px;
+    }
+
+    &__soc-link {
+      margin: 0px 10px;
+    }
+  }
+
+
+  .phone-row {
+    margin: -15px;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+
+    &__phone {
+      margin: 0 15px;
+      @include text(p1);
+      white-space: nowrap;
+    }
+
+    &__soc-icon {
+      margin: 15px;
+
+      img {
+        width: 24px;
+      }
+    }
+
+    &_mini &__phone {
+      font-size: 18px;
+    }
+
+    &_mini &__soc-icon {
+      margin: 10px;
+      display: flex;
+      align-items: center;
+
+      img {
+        width: 21px;
+      }
+    }
+  }
+</style>
