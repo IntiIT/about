50787f462929d3e51ca4f68d697e748581bc0a74
Efim <42698670+Efim27@users.noreply.github.com>
2023-03-29 22:45:01 +0300

    adaptive slider

--- frontend/src/lib/components/slider/Slider.svelte
+++ frontend/src/lib/components/slider/Slider.svelte
@@ -10,27 +10,33 @@
 
 <svelte:component autoplay autoplayDuration={5000} this={Carousel} let:showPrevPage let:showNextPage>
     <button
-        type="button"
-        slot="prev"
-        on:click={showPrevPage}
-        class="nav-btn"
+            type="button"
+            slot="prev"
+            on:click={showPrevPage}
+            class="nav-btn"
     >
         &LongLeftArrow;
     </button>
     <slot/>
     <button
-        type="button"
-        slot="next"
-        on:click={showNextPage}
-        class="nav-btn"
+            type="button"
+            slot="next"
+            on:click={showNextPage}
+            class="nav-btn"
     >
         &LongRightArrow;
     </button>
 </svelte:component>
 
-<style>
+<style lang="scss">
+  .nav-btn {
+    background: none;
+    border: none;
+  }
+
+  @media screen and (max-width: 992px) {
     .nav-btn {
-        background: none;
-        border: none;
+      display: none;
     }
+  }
 </style>
